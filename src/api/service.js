602bc0d009a1e9d9448836bfc93c392d182b9bea
carolingoerner <132401696+carolingoerner@users.noreply.github.com>
2023-07-06 19:36:35 +0200

    gutten tag

--- src/api/service.js
+++ src/api/service.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const api = axios.create({
   // make sure you use PORT = 5005 (the port where our server is running)
-  baseURL: "http://localhost:5005/api"
+  baseURL: process.env.REACT_APP_SERVER_URL
   // withCredentials: true // => you might need this option if using cookies and sessions
 });
 
